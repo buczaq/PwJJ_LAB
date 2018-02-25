e85cfc60322cac3194a7460ed3b6d3a365c6931d
buczaq <buczaq@gmail.com>
2018-02-25 16:08:20 +0100

    init commit

---
+++ Testatistics/src/testatistics/Testatistics.java
@@ -0,0 +1,13 @@
+
+package testatistics;
+
+/**
+ *
+ * @author buczak
+ */
+public class Testatistics {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
